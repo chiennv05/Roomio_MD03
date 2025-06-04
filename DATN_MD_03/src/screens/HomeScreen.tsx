057a345379d0200565ef11fc24178c373373db93
las-tonbd <tonbd@lasbom.com>
2025-06-04 22:09:45 +0700

    commit

--- DATN_MD_03/src/screens/HomeScreen.tsx
+++ DATN_MD_03/src/screens/HomeScreen.tsx
@@ -1,9 +1,12 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
-import HomeScreen from './src/screens/HomeScreen';
 
-export default function App() {
-  return <HomeScreen />;
+export default function HomeScreen() {
+  return (
+    <View>
+      <Text>HomeScreen</Text>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({});
