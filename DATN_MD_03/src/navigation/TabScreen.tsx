3009c05cf2399c859d6a5da204aebea969428ba6
las-tonbd <tonbd@lasbom.com>
2025-06-10 21:17:57 +0700

    UI : home screen

--- DATN_MD_03/src/navigation/TabScreen.tsx
+++ DATN_MD_03/src/navigation/TabScreen.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import {RootStackParamList} from '../types/route';
-import HomeScreen from '../screens/HomeScreen';
+
 import SplashScreen from '../screens/Splash/SplashScreen';
 import LoginAndRegister from '../screens/LoginAndRegister/LoginAndRegister';
 import {NavigationContainer} from '@react-navigation/native';
+import HomeScreen from '../screens/HomeScreen/HomeScreen';
 
 const Stack = createStackNavigator<RootStackParamList>();
 export default function TabScreen() {
